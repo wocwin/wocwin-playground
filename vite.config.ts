7e5eb260fb139fab494a5c01585b181b73e68419
wocwin <wocwin@163.com>
2025-06-06 21:28:27 +0800

    fix:禁用不必要的插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,40 @@
-import fs from 'node:fs'
+// import fs from 'node:fs'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { chunkSplitPlugin } from 'vite-plugin-chunk-split'
-import viteCompression from 'vite-plugin-compression'
+// import { chunkSplitPlugin } from 'vite-plugin-chunk-split'
+// import viteCompression from 'vite-plugin-compression'
 
 // 生产环境判断
-const isEnvProduction = process.env.NODE_ENV === 'production'
+// const isEnvProduction = process.env.NODE_ENV === 'production'
 
 export default defineConfig({
   base: "/wocwin-playground/",
   server: {
+    open: true,
+    host: '0.0.0.0',
     port: 3332
   },
   plugins: [
-    vue({
-      script: {
-        defineModel: true,
-        fs: {
-          fileExists: fs.existsSync,
-          readFile: file => fs.readFileSync(file, 'utf-8')
-        }
-      }
-    }),
-    chunkSplitPlugin({
-      strategy: 'single-vendor'
-    }),
-    isEnvProduction
-      ? viteCompression({
-          filter: /\.(js|css)$/i,
-          // algorithm: 'brotliCompress',
-          threshold: 10 * 1024 // 10kb
-        })
-      : undefined
+    vue()
+    // vue({
+    //   script: {
+    //     defineModel: true,
+    //     fs: {
+    //       fileExists: fs.existsSync,
+    //       readFile: file => fs.readFileSync(file, 'utf-8')
+    //     }
+    //   }
+    // }),
+    // chunkSplitPlugin({
+    //   strategy: 'single-vendor'
+    // }),
+    // isEnvProduction
+    //   ? viteCompression({
+    //       filter: /\.(js|css)$/i,
+    //       // algorithm: 'brotliCompress',
+    //       threshold: 10 * 1024 // 10kb
+    //     })
+    //   : undefined
   ],
   define: {
     __VUE_PROD_DEVTOOLS__: JSON.stringify(true)
